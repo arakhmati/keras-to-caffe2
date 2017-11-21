8ea78d323f14e65e81979f3f400083757ddc1c88
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2017-11-20 23:18:35 -0500

    Set use_cudnn to False. Add all the parameters of spatial_bn to params list of the model

--- keras_to_caffe2/keras_to_caffe2.py
+++ keras_to_caffe2/keras_to_caffe2.py
@@ -7,14 +7,13 @@ from caffe2.proto import caffe2_pb2
 
 def copy_model(keras_model):
     
-    arg_scope = {'order': 'NCHW'}
+    arg_scope = {'order': 'NCHW', 'use_cudnn': False}
     caffe2_model = model_helper.ModelHelper(name='model', arg_scope=arg_scope)
     
     prev_name = 'in'
     prev_shape = None
     
     for keras_layer in keras_model.layers:
-        
         config = keras_layer.get_config()
         
         if 'data_format' in config:
@@ -27,8 +26,9 @@ def copy_model(keras_model):
         else:
             shape = prev_shape
         
+        name = config['name']
+
         if isinstance(keras_layer, keras.layers.Conv2D):
-            name = config['name']
             dim_in = shape[0]
             dim_out = config['filters']
             kernel = config['kernel_size'][0]
@@ -54,7 +54,7 @@ def copy_model(keras_model):
         elif isinstance(keras_layer, keras.layers.BatchNormalization):
             if not isinstance(shape, list):
                 continue
-            name = config['name']
+
             dim_in = shape[0]
             epsilon = config['epsilon']
             momentum = config['momentum']
@@ -68,8 +68,6 @@ def copy_model(keras_model):
             prev_name = name
             
         elif isinstance(keras_layer, keras.layers.MaxPooling2D):
-            
-            name = config['name']
             kernel = config['pool_size'][0]
             stride = config['strides'][0]
             brew.max_pool(caffe2_model,
@@ -83,8 +81,7 @@ def copy_model(keras_model):
         elif isinstance(keras_layer, keras.layers.Flatten):
             prev_shape = shape[0] * shape[1] * shape[2]
             
-        elif isinstance(keras_layer, keras.layers.Dense):     
-            name = config['name']           
+        elif isinstance(keras_layer, keras.layers.Dense):
             dim_in = shape
             dim_out = config['units']
             
@@ -105,7 +102,7 @@ def copy_model(keras_model):
             
     return caffe2_model
 
-def copy_weights(keras_model):
+def set_weights(keras_model, caffe2_model):
     
     for keras_layer in keras_model.layers:
         
@@ -118,7 +115,6 @@ def copy_weights(keras_model):
             workspace.FeedBlob(name + '_b', b)
             
         elif isinstance(keras_layer, keras.layers.BatchNormalization):
-            
             s = keras_layer.get_weights()[0]
             b = keras_layer.get_weights()[1]
             rm = keras_layer.get_weights()[2]
@@ -129,6 +125,10 @@ def copy_weights(keras_model):
             workspace.FeedBlob(name + '_rm', rm)
             workspace.FeedBlob(name + '_riv', riv)
             
+            # Add rm and riv parameters of spatial_bn layers to params list of the model
+            caffe2_model.params.append(workspace.StringifyBlobName(name + '_rm'))
+            caffe2_model.params.append(workspace.StringifyBlobName(name + '_riv'))
+
         elif isinstance(keras_layer, keras.layers.Dense):     
             w = keras_layer.get_weights()[0].transpose()
             b = keras_layer.get_weights()[1]
@@ -149,6 +149,6 @@ def keras_to_caffe2(keras_model):
     workspace.RunNetOnce(caffe2_model.param_init_net)
     workspace.CreateNet(caffe2_model.net)
     
-    copy_weights(keras_model)
+    set_weights(keras_model, caffe2_model)
     
     return caffe2_model
